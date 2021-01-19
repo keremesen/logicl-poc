9e26c3daf9ea8d50ce4fd94a8f4b71dbd020cca2
unallsukru <unallsukru@gmail.com>
2021-01-19 23:25:48 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sea  <code>src/App.js</code> and save to reload.
+         xd
         </p>
         <p>
           test
