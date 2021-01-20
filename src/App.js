5b692c8a79aae7340667c54a134540a0bebf80c8
Kerem Esen <kerem.esen.01@hotmail.com>
2021-01-21 00:30:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import { Flex } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import ExplorePage from "./Pages/ExplorePage";
-import Main from "./Pages/Main";
-import Auth from "./Pages/Auth";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { Flex, Heading } from "@chakra-ui/react";
+// import NavBar from "./components/NavBar";
+// import ExplorePage from "./Pages/ExplorePage";
+// import Main from "./Pages/Main";
+// import Auth from "./Pages/Auth";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <NavBar />
-        <Flex
-          w="100vw"
-          direction="row"
-          align="center"
-          justify="center"
-          height="auto"
-          bgColor="#CBD5E0"
-        >
-          <Switch>
-            <Route path="/" exact>
-              <Main />
-            </Route>
+    // <Router>
+    //   <AuthProvider>
+    //     <NavBar />
+    //     <Flex
+    //       w="100vw"
+    //       direction="row"
+    //       align="center"
+    //       justify="center"
+    //       height="auto"
+    //       bgColor="#CBD5E0"
+    //     >
+    //       <Switch>
+    //         <Route path="/" exact>
+    //           <Main />
+    //         </Route>
 
-            <Route path="/explore">
-              <ExplorePage />
-            </Route>
+    //         <Route path="/explore">
+    //           <ExplorePage />
+    //         </Route>
 
-            <Route path="/auth">
-              <Auth />
-            </Route>
-          </Switch>
-        </Flex>
-      </AuthProvider>
-    </Router>
+    //         <Route path="/auth">
+    //           <Auth />
+    //         </Route>
+    //       </Switch>
+    //     </Flex>
+    //   </AuthProvider>
+    // </Router>
+
+    <Flex
+      w="100vw"
+      h="100vh"
+      backgroundColor="gray.200"
+      align="center"
+      justify="center"
+    >
+      <Heading size="4xl">coming soon..</Heading>
+    </Flex>
   );
 }
 
