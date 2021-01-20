3d9bfad90d4f66fab68930aae07649df5ac4b67b
EmreMertA <emremakdag55@gmail.com>
2021-01-20 17:13:11 +0300

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Flex, flexbox, Heading } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar/>
       <Flex
         direction="row"
         align="center"
@@ -13,6 +15,7 @@ function App() {
         height="100vh"
         bgColor="#CBD5E0"
       >
+      
         <Heading size="xl" color='#2D3748'>Coming Soon</Heading>
       </Flex>
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,27 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Avatar, AvatarBadge, Heading, Stack } from "@chakra-ui/react";
+import { BellIcon, AddIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
-    return (
-        <Flex as="nav" >
+  return (
+    <Flex
+      as="nav"
+      w="100vw"
+      h="64px"
+      bgColor="#FEEBC8"
+      align="center"
+      justify="space-between"
+      px="32px"
+    >
+      <Flex>
+        <Heading>logicl</Heading>
+      </Flex>
+      <Stack direction="row" spacing="16px" align="center">
+        <AddIcon boxSize={6} />
+        <BellIcon boxSize={8} />
+        <Avatar boxSize={8} bgColor="#000" />
+      </Stack>
+    </Flex>
+  );
+};
 
-        </Flex>
-    )
-}
-
-export default NavBar
+export default NavBar;
