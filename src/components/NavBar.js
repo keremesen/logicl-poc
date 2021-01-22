17e9f2aea20edd889c4d54a55513f67b0b6ca51f
msinant <mehmetsinan_topal42@hotmail.com>
2021-01-22 22:17:52 +0300

    edited navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,26 +11,37 @@ const NavBar = () => {
       w="100vw"
       h="7vh"
       bgColor="#FEEBC8"
-      align="center"
-      justify="space-between"
-      px="32px"
+      justifyContent="center"
     >
-      <Flex>
-        <Heading>
-          <Link to="/">logicl</Link>
-        </Heading>
-      </Flex>
-      <Button backgroundColor="teal.100" w="100px">
-        <Link to="/explore">Explore</Link>
-      </Button>
-      <Stack direction="row" spacing="16px" align="center">
-        <AddIcon boxSize={6} />
-        <BellIcon boxSize={8} />
+      <Flex
+        w="1080px"
+        h="7vh"
+        bgColor="#FEEBC8"
+        align="center"
+        justify="space-between"
+        px="8px"
+      >
+        <Flex>
+          <Heading>
+            <Link to="/" style={{padding:'8px 16px 8px 0px'}}>logicl</Link>
+          </Heading>
+        </Flex>
+        <Button backgroundColor="teal.100" w="100px">
+          <Link to="/explore" style={{padding:'12px 22px'}}>Explore</Link>
+        </Button>
+        <Stack direction="row" spacing="16px" align="center">
+          <AddIcon boxSize={6} />
+          <BellIcon boxSize={8} />
 
-        <Link to="/auth">
-          <Avatar boxSize={8} bgColor="#000" src={auth.user ? auth.user.photoUrl : ''} />
-        </Link>
-      </Stack>
+          <Link to="/auth">
+            <Avatar
+              boxSize={8}
+              bgColor="#000"
+              src={auth.user ? auth.user.photoUrl : ""}
+            />
+          </Link>
+        </Stack>
+      </Flex>
     </Flex>
   );
 };
