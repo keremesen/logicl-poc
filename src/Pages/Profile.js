4fdb5568c85db8dfea6d86a6de0d7263e6f57db1
Kerem Esen <kerem.esen.01@hotmail.com>
2021-01-29 21:27:45 +0300

    update profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,10 +1,19 @@
-import { Avatar, Button, Flex, Heading } from "@chakra-ui/react";
-import React, { useEffect, useRef } from "react";
+import { Avatar, Button, Flex, FormControl, FormLabel, Heading, Input, VisuallyHidden } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
 import FullScreenSpinner from "../components/FullScreenSpinner";
 import { useAuth } from "../context/AuthContext";
+import Auth from "./Auth";
+
 
 const Profile = (props) => {
-  const { user, loading, signout } = useAuth();
+
+  const { user, loading } = useAuth();
+ 
+const [read, setRead] = useState(true)
+
+const [name , setName] = useState(user.name)
+const [email , setEmail] = useState(user.email)
+
 
   const isMounted = useRef(null);
 
@@ -26,6 +35,7 @@ const Profile = (props) => {
     return <FullScreenSpinner />
   }
 
+
   return (
     <Flex
       w="1080px"
@@ -37,10 +47,24 @@ const Profile = (props) => {
     >
       <Heading m={5}>Profile</Heading>
       <Flex direction="row" m={6}>
+        <Flex direction="column">
         <Avatar boxSize={48} bgColor="#000" src={user.photoUrl} />
-        <Flex direction="column" m={8}>
-          <Heading>{user.name}</Heading>
-          <Heading>{user.email}</Heading>
+        <Button bgColor="gray.200" mt="15px">Change Photo</Button>
+        </Flex>
+        
+        <Flex direction="column" m={8} w="320px">
+          
+          <FormLabel mt="30px">NAME</FormLabel>
+         <Input value={name} isReadOnly={read} onChange={(e)=> {setName(e.target.value)}} /> 
+         <FormLabel>EMAIL</FormLabel>
+         <FormControl id="email" isRequired>
+         <Input value={email} isReadOnly={read} onChange={(e)=> {setEmail(e.target.value)}}  type="email" /> 
+         </FormControl>
+         
+         <Button bgColor="gray.200" mt="15px" onClick={()=> setRead(false)} isDisabled={!read} > Set Profile</Button>
+         {read ? ''  : <Button bgColor="green.300" mt={4} onClick={()=> setRead(true)}  >Save</Button>}
+         
+         
         </Flex>
       </Flex>
     </Flex>
